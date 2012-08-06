a56151e05f5cc19f1d64a83b07056a64ae8e0c7d
Carlos Hugo Gonzalez Castell <caarloshugo@gmail.com>
2012-08-06 12:47:26 -0500

    faltas de ortografia

--- www/applications/default/views/home.php
+++ www/applications/default/views/home.php
@@ -136,7 +136,7 @@
 				<div id="desc-registro">
 					<span>
 						Llena los datos que te piden para completar tu registro.<br />
-						Recibir&oacute;s confirmaci&oacute;n v&iacute;a email al realizar un registro exitoso.
+						Recibir&aacute;s confirmaci&oacute;n v&iacute;a email al realizar un registro exitoso.
 					</span>
 				</div>
 				
