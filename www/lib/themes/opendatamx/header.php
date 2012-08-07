e2343f997a15102134b8c829090a29326ec83dba
Carlos Hugo Gonzalez Castell <caarloshugo@gmail.com>
2012-08-07 00:02:06 -0500

    agregando menu de registro en el header

--- www/lib/themes/opendatamx/header.php
+++ www/lib/themes/opendatamx/header.php
@@ -83,7 +83,8 @@
 					<div id="logo-menu"><a href="#opendata"><img src="<?php print $this->themePath; ?>/css/images/logo-opendata.png" title="OpenDataMX"/></a></div>
 					<ul id="menu-ul">
 						<li><a href="#programa" title="Programa">Programa</a></li>
-						<li><a href="#registro" title="Registro">Registro</a></li>
+						<li><a href="<?php print get('webURL'); ?>/organizaciones" title="Registro Organizaciones">Registro Osc's</a></li>
+						<li><a href="#registro" title="Registro Hackers">Registro Hackers</a></li>
 						<li><a href="#necesidades" title="Ejemplos de retos">Retos</a></li>
 						<li><a href="#data-sets" title="Data Sets">Data Sets</a></li>
 						<li><a href="<?php print get('webURL'); ?>/blog" title="OpenData Blog">Blog</a></li>
--- www/lib/themes/opendatamxblog/header.php
+++ www/lib/themes/opendatamxblog/header.php
@@ -33,7 +33,7 @@
 				<div id="logo-menu"><a href="<?php print get('webURL'); ?>"><img src="<?php print $this->themePath; ?>/css/images/logo-opendata.png" title="OpenDataMX"/></a></div>
 				<ul id="menu-ul">
 					<li><a href="<?php print get('webURL'); ?>#programa" title="Programa">Programa</a></li>
-					<li><a href="<?php print get('webURL'); ?>#registro" title="Registro">Registro</a></li>
+					<li><a href="<?php print get('webURL'); ?>#registro" title="Registro Hackers">Registro Hackers</a></li>
 					<li><a href="<?php print get('webURL'); ?>#necesidades" title="Necesidades sociales">Necesidades Sociales</a></li>
 					<li><a href="<?php print get('webURL'); ?>#data-sets" title="Data Sets">Data Sets</a></li>
 					<li><a href="<?php print get('webURL'); ?>/blog" title="OpenData Blog">Blog</a></li>
