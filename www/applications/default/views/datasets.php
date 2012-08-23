a17b1eecdec882b47ada0ad5201b68f78e3f861f
Carlos Hugo Gonzalez Castell <caarloshugo@gmail.com>
2012-08-23 12:21:56 -0500

    anclas de los datasets

--- www/applications/default/views/datasets.php
+++ www/applications/default/views/datasets.php
@@ -6,49 +6,49 @@
 <p>Base de datos del Programa de Educaci&oacute;n Preescolar y Primaria para Ni&ntilde;as y Ni&ntilde;os de Familias Jornaleras Agr&iacute;colas Migrantes, PRONIM 2010-2012</p>
 <p><a href="https://github.com/downloads/opendatamx/datasetsagosto2012/Pronim.zip" title="DataSet">Descargar</a></p>
 
-<h2>Pronabes</h2>
+<h2><a name="pronabes"></a>Pronabes</h2>
 <p>Base de datos de becarios - ciclo escolar 2006-2007</p>
 <p><a href="https://github.com/downloads/opendatamx/datasetsagosto2012/Pronabes.xls" title="DataSet">Descargar</a></p>
 
-<h2>Programa Escuela Segura</h2>
+<h2><a name="programa-escuela-segura"></a>Programa Escuela Segura</h2>
 <p>Base de datos de escuelas que participan en el programa escuela segura, &uacute;ltima fecha de actualizaci&oacute;n 24 de enero de 2011</p>
 <p><a href="https://github.com/downloads/opendatamx/datasetsagosto2012/ProgramaEscuelaSegura.xls" title="DataSet">Descargar</a></p>
 
-<h2>Alianza Infraestructura</h2>
+<h2><a name="alianza-infraestructura"></a>Alianza Infraestructura</h2>
 <p>Escuelas con mejoras realizadas mediante el Programa de Fortalecimiento a la Infraestructura y el equipamiento a 2011</p>
 <p><a href="https://github.com/downloads/opendatamx/datasetsagosto2012/BaseAlianzainfraestructura.xls" title="DataSet">Descargar</a></p>
 
-<h2>Listado de cr&eacute;ditos fiscales</h2>
+<h2><a name="creditos-fiscales"></a>Listado de cr&eacute;ditos fiscales</h2>
 <p>Cr&eacute;ditos otorgados por el SAT a deudores de impuestos<br>
  Fuente: Solicitudes de informaci&oacute;n Fundar</p>
 <p><a href="https://github.com/downloads/opendatamx/datasetsagosto2012/Listado_creditos.xlsx" title="DataSet">Descargar</a></p>
 
-<h2>&Iacute;ndice de Desempe&ntilde;o de los Programas Federales de Subsidio</h2>
+<h2><a name="desempeno-subsidios-federales"></a>&Iacute;ndice de Desempe&ntilde;o de los Programas Federales de Subsidio</h2>
 <p>Los Programas comprendidos en el INDEP 2011 son aquellos programas federales de subsidios (sujetos a reglas de operaci&oacute;n y de otros subsidios) y de prestaci&oacute;n de servicios p&uacute;blicos, que de manera proactiva hacen transparente informaci&oacute;n socialmente &uacute;til</p>
 <p><a href="https://github.com/downloads/opendatamx/datasetsagosto2012/INDEP.zip" title="DataSet">Descargar</a></p>
 
-<h2>Solicitudes de informaci&oacute;n INFO-DF 2006-2009</h2>
+<h2><a name="info-df"></a>Solicitudes de informaci&oacute;n INFO-DF 2006-2009</h2>
 <p>Fuente: Instituto de Acceso a la Informaci&oacute;n P&uacute;blica y Protecci&oacute;n de Datos Personales del Distrito Federal</p>
 <p><a href="https://github.com/downloads/opendatamx/datasetsagosto2012/DataSetSolicitudes_INFO-DF.xlsx" title="DataSet">Descargar</a></p>
 
-<h2>&Iacute;ndice de Transparencia en Sustentabilidad Corporativa (ITSC)</h2>
+<h2><a name="itsc"></a>&Iacute;ndice de Transparencia en Sustentabilidad Corporativa (ITSC)</h2>
 <p>Fuente: GESO</p>
 <p><a href="https://github.com/downloads/opendatamx/datasetsagosto2012/BASE%20ITSC%20VF.xlsx" title="DataSet">Descargar</a></p>
 
-<h2>Presupuesto Federal 2011</h2>
+<h2><a name="presupuesto-federal-2011"></a>Presupuesto Federal 2011</h2>
 <p>Fuente: Secretar&iacute;a de hacienda y cr&eacute;dito p&uacute;blico</p>
 <p><a href="https://github.com/downloads/opendatamx/datasetsagosto2012/Analitico-2011.csv" title="DataSet">Descargar</a></p>
 
-<h2>Padrones beneficiarios financiamiento a bosques</h2>
+<h2><a name="padron-financiamiento-bosques"></a>Padrones beneficiarios financiamiento a bosques</h2>
 <p>El dataset tiene información por municipio, superficie apoyada y monto, desagregado por programa<br>Fuente: Consejo Civil Mexicano para la Silvicultura Sostenible</p>
 <p><a href="https://github.com/downloads/opendatamx/datasetsagosto2012/padrones_beneficiarios_financiamiento_a_bosques.xls" title="DataSet">Descargar</a></p>
 
-<h2>Compras de software</h2>
+<h2><a name="compras-software"></a>Compras de software</h2>
 <p>Fuente: Solicitudes de Informaci&oacute;n<br>
  &iquest;Cu&aacute;ntas licencias para Software fueron adquiridas en 2009 y 2010?</p>
 <p><a href="https://github.com/downloads/opendatamx/datasetsagosto2012/compras-software.zip" title="DataSet">Descargar</a></p>
 
-<h2>Base de datos ahorro y financiamiento CNBV</h2>
+<h2><a name="ahorro-financiamiento"></a>Base de datos ahorro y financiamiento CNBV</h2>
 <p>Fuente: Comisi&oacute;n Nacional Bancaria y de Valores<br>
 Esta base de datos ha sido elaborada por la Direcci&oacute;n General de Estudios Econ&oacute;micos con el objetivo de poner a disposici&oacute;n del p&uacute;blico informaci&oacute;n agregada, confiable, completa y f&aacute;cil de utilizar, sobre los instrumentos de ahorro y financiamiento que mantienen en su balance todos los intermediarios financieros supervisados por &eacute;sta y otras autoridades. <br>
 <br>
@@ -56,50 +56,50 @@ Dicha informaci&oacute;n contiene los saldos trimestrales del ahorro y el financ
 </p>
 <p><a href="https://github.com/downloads/opendatamx/datasetsagosto2012/Base%20de%20datos%20ahorro%20y%20financiamiento%20CNBV.xls" title="DataSet">Descargar</a></p>
 
-<h2>SETEC Subsidios por eje</h2>
+<h2><a name="setec"></a>SETEC Subsidios por eje</h2>
 <p>Solicitud de la distribuci&oacute;n de recursos por eje rector de los subsidios que otorg&oacute; la SETEC en 2010</p>
 <p><a href="https://github.com/downloads/opendatamx/datasetsagosto2012/SETEC_Subsidios%20por%20eje.xls" title="DataSet">Descargar</a></p>
 
-<h2>Presupuesto general del poder judicial de la federaci&oacute;n</h2>
+<h2><a name="presupuesto-judicial"></a>Presupuesto general del poder judicial de la federaci&oacute;n</h2>
 <p>Fuente: Presupuesto de Egresos de la Federaci&oacute;n www.shcp.gob.mx</p>
 <p><a href="https://github.com/downloads/opendatamx/datasetsagosto2012/EVOLUCION%20presupuesto%20PJF2000_2012.xlsx" title="DataSet">Descargar</a></p>
 
-<h2>Procampo 94-09</h2>
+<h2><a name="procampo"></a>Procampo 94-09</h2>
 <p>Fuente: SAGARPA<br>
  Base de datos del programa Procampo</p>
 <p><a href="https://github.com/downloads/opendatamx/datasetsagosto2012/BDProcampo_94-09.zip" title="DataSet">Descargar</a></p>
 
-<h2>Poblaci&oacute;n municipal conteo 2005</h2>
+<h2><a name="poblacion-municipal"></a>Poblaci&oacute;n municipal conteo 2005</h2>
 <p>Estadsticas que el INEGI genera o recaba, con desagregación por estado y municipio</p>
 <p><a href="Descargar" title="DataSet">Descargar</a></p>
 
-<h2>Hist&oacute;rico de periodistas asesinados</h2>
+<h2><a name="periodistas-asesinados"></a>Hist&oacute;rico de periodistas asesinados</h2>
 <p>Base de datos con nombres, localidad y fecha de los periodistas asesinados<br>
  Fuente: Articulo19</p>
 <p><a href="https://github.com/downloads/opendatamx/datasetsagosto2012/Historicodeasesinatos2000.xls" title="DataSet">Descargar</a></p>
 
-<h2>Hist&oacute;rico de periodistas desaparecidos</h2>
+<h2><a name="periodistas-desaparecidos"></a>Hist&oacute;rico de periodistas desaparecidos</h2>
 <p>Base de datos con nombres, localidad y fecha de los periodistas desaparecidos<br>
 Fuente: Articulo19</p>
 <p><a href="https://github.com/downloads/opendatamx/datasetsagosto2012/Historicodedesapariciones.xls" title="DataSet">Descargar</a></p>
 
-<h2>Incidencia delictiva seg&uacute;n el Sistema Nacional de Seguridad P&uacute;blica (Formato CIEISP)</h2>
+<h2><a name="indidencia-delictiva"></a>Incidencia delictiva seg&uacute;n el Sistema Nacional de Seguridad P&uacute;blica (Formato CIEISP)</h2>
 <p>Fuente: Elaboraci&oacute;n propia con base en informaci&oacute;n del Sistema Nacional de Seguridad P&uacute;blica (Formato CIEISP)</p>
 <p><a href="https://github.com/downloads/opendatamx/datasetsagosto2012/denun.xls" title="DataSet">Descargar</a></p>
 
-<h2>Fallecimientos ocurridos por presunta rivalidad delincuencial 2006-2010</h2>
+<h2><a name="fallecimientos-2006-2010"></a>Fallecimientos ocurridos por presunta rivalidad delincuencial 2006-2010</h2>
 <p>Base de datos de fallecimientos ocurridos por presunta rivalidad delincuencial en el periodo 2006-2010</p>
 <p><a href="https://github.com/downloads/opendatamx/datasetsagosto2012/baseFallecimientosRD.xlsx" title="DataSet">Descargar</a></p>
 
-<h2>&Iacute;ndice de cuadros prospectiva de petrol&iacute;feros 2010-2025</h2>
+<h2><a name="prospectiva-petroleo"></a>&Iacute;ndice de cuadros prospectiva de petrol&iacute;feros 2010-2025</h2>
 <p>Fuente: Sener, SIE con base en informaci&oacute;n de Pemex Refinaci&oacute;n</p>
 <p><a href="https://github.com/downloads/opendatamx/datasetsagosto2012/CuadrosEstadisticosProspectivaPetroliferos2010-2025.xls" title="DataSet">Descargar</a></p>
 
-<h2>Encuesta Nacional de Lectura</h2>
+<h2><a name="encuesta-nacional-lectura"></a>Encuesta Nacional de Lectura</h2>
 <p>Encuesta Nacional de Lectura y es importante se&ntilde;alar lo siguiente: La base de datos de la encuesta está en el formato del paquete estad&iacute;stico SPSS (.sav)</p>
 <p><a href="https://github.com/downloads/opendatamx/datasetsagosto2012/BD_ENL.zip" title="DataSet">Descargar</a></p>
 
-<h2>Resultados de prueba Enlace(SEP) por escuela preescolar, primaria y secundaria 2006-2010 (SQL)</h2>
+<h2><a name="enlace-sep"></a>Resultados de prueba Enlace(SEP) por escuela preescolar, primaria y secundaria 2006-2010 (SQL)</h2>
 <p>Base de datos de la prieba de Enlace de la SEP del periodo 2006-2010</p>
 <p><a href="https://github.com/downloads/opendatamx/datasetsagosto2012/bckcte.zip" title="DataSet">Descargar</a></p>
 
